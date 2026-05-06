5bd1d9f5659353ffde331fa4476d69569995da47
vlad digori <digorivlad59@gmail.com>
2026-05-06 23:13:06 +0100

    `refactor: update encoding to 'utf-8' in git_utils.py`

--- komit/git_utils.py
+++ komit/git_utils.py
@@ -2,24 +2,26 @@
 import subprocess
 
 def get_staged_diff() -> str:
-    result = subprocess.run(['git', 'diff', '--staged'], capture_output=True,text=True)
+    result = subprocess.run(['git', 'diff', '--staged'], capture_output=True,text=True,encoding='utf-8')
     return result.stdout
 
 def get_staged_files() -> list[str]:
-    result = subprocess.run(['git', 'diff', '--staged', '--name-only'], capture_output=True,text=True)
+    result = subprocess.run(['git', 'diff', '--staged', '--name-only'], capture_output=True,text=True,encoding='utf-8')
     return [f for f in result.stdout.strip().split('\n') if f]
 
 def commit(message:str):
     subprocess.run(
-        ['git','commit','-m',message],check=True
+        ['git','commit','-m',message],check=True,encoding='utf-8'
     )
 
 def commit_with_editor(message:str):
     subprocess.run(
         ['git','commit','-m',message,'-e'],
-        check=True
+        check=True,
+        text=True
+        ,encoding='utf-8'
     )
 
 def is_git_repo():
-    result = subprocess.run(['git','rev-parse','--is-inside-work-tree'],capture_output=True,text=True)
+    result = subprocess.run(['git','rev-parse','--is-inside-work-tree'],capture_output=True,text=True,encoding='utf-8')
     return result.returncode == 0
